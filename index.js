6be0d526c83a9702e098684a2db78d40a9618402
Rubayth Haque <rubayth19@gmail.com>
2019-04-18 16:34:44 -0400

    started implementation of google oauth

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const express = require('express');
-const app = express();
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 
-app.get('/', (req, res) => {
-    res.send({hi: 'there'});
-});
+const app = express();
+//http://localhost:5000/auth/google/callback
+
+passport.use
+    (new GoogleStrategy({
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: '/auth/google/callback'
+    }, (accessToken) => {
+        console.log(accessToken)
+    })
+);
+
+
 
 
 app.listen(process.env.PORT || 5000);
+
