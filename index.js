f9c99d621d7abdf61bb34302edc8d16f0a69c8f5
Rubayth Haque <rubayth@Rubayths-MacBook-Pro.local>
2019-04-18 21:15:04 -0400

    authentication, sessions

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('./config/keys');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+require('./models/User'); //must require this before passport.js (below)
+require('./services/passport');
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
-//http://localhost:5000/auth/google/callback
 
-passport.use
-    (new GoogleStrategy({
-        clientID: keys.googleClientID,
-        clientSecret: keys.googleClientSecret,
-        callbackURL: '/auth/google/callback'
-    }, (accessToken) => {
-        console.log(accessToken)
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
     })
 );
+app.use(passport.initialize());
+app.use(passport.session());
 
-
+require('./routes/authRoutes')(app);
 
 
 app.listen(process.env.PORT || 5000);
