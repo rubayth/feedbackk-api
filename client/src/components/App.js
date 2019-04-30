375374390b26cbd72b62242bb7229c8a68eed6d2
Rubayth Haque <rubayth@Rubayths-MacBook-Pro.local>
2019-04-29 22:02:05 -0400

    react router initial

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,13 +1,34 @@
 import React, {Component} from 'react';
 import { Button } from 'react-bootstrap';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
+
+const Header = () => <h2>Header</h2>
+const Dashboard = () => <h2>Dashboard</h2>
+const SurveyNew = () => <h2>SurveyNew</h2>
+const Landing = () => 
+  <div>
+    <h1>Feedbackk</h1>
+    <Button variant="primary" href=" /auth/google"> Sign in with Google</Button>
+  </div>
+
+  
+  
+
+
 
 class App extends Component {
   render(){
     return(
       <div className="App">
-      <h1>Feedbackk</h1>
-        <Button variant="primary" href=" /auth/google"> Sign in with Google</Button>
+      <BrowserRouter>
+        <div>
+          <Header/>
+          <Route exact path="/" component={Landing}/>
+          <Route exact path="/surveys" component={Dashboard}/>
+          <Route path="/surveys/new" component={SurveyNew}/>
+        </div>
+      </BrowserRouter>
       </div>
     );
   }
