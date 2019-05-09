fe3f6a2794942e01275e888f5a987302a5887578
Rubayth Haque <rubayth19@gmail.com>
2019-05-09 17:22:53 -0400

    deployment

--- index.js
+++ index.js
@@ -25,6 +25,14 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
+if(process.env.NODE_ENV === 'production') {
+    //express will serve production assets
+    app.use(express.static('client/build'));
 
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 app.listen(process.env.PORT || 5000);
 
